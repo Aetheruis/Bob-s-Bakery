87bf017df9aac66281dc04b11be248d0cecf6fdd
Bernard <hbernardgardner@gmail.com>
2022-08-11 20:05:12 +0200

    This is just to test

--- php_program/welcome.php
+++ php_program/welcome.php
@@ -58,7 +58,7 @@ require_once "config.php";
         .topnav a.split {
             float: right;
             background-color: #04AA6D;
-            color: white;
+            color: black;
         }
     </style>
     
